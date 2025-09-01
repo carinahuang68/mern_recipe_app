8479c649a469a2bebbaa4b3f323bd3a515a7c7ac
Carina Huang <huangc24@students.cs.ubc.ca>
2025-09-01 15:23:33 -0700

    deploy setup

--- backend/server.js
+++ backend/server.js
@@ -4,7 +4,7 @@ import { connectDB } from './config/db.js';
 import recipeRoutes from './routes/recipe.route.js';
 import authRoutes from './routes/auth.js';
 import cors from 'cors';
-
+import path from 'path';
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -14,6 +14,15 @@ app.use(express.json()); // allows us to accept JSON data in req.body
 
 app.use('/api/auth', authRoutes);
 app.use('/api/recipes', recipeRoutes);
+
+const __dirname = path.resolve();
+
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, "/frontend/dist"))); //makes content of dist folder available to client via http requests
+    app.get("/{*splat}", (req, res) => {
+        res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));    
+    })
+}
 
 
 app.listen(PORT, async () => {
